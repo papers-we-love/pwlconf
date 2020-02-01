247d49dfa93575261187b499beda67e1549ca732
DarrenN <info@v25media.com>
2020-02-01 16:16:17 -0500

    Sets up directories for 2020
    - 2020 logo created
    - Creates 2020 folder with prospectus and partials
    - Added index to 2019 folder1

--- config.rb
+++ config.rb
@@ -37,6 +37,11 @@ with_layout :pwlconf2019 do
   page "/"
 end
 
+with_layout :pwlconf2019 do
+  page "/2020/*"
+end
+
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
